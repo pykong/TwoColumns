5106e16a8007daf8ccf6071ae9a43b818b372c4c
bfelder <bfelder@users.noreply.github.com>
2017-09-10 22:24:42 +0200

    Made set_two_columns independent function.

--- TwoColumns.py
+++ TwoColumns.py
@@ -7,37 +7,31 @@ TWO_COL_LAYOUT = {
     "cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
 }
 
-SHARE_OBJECT = 'max_pane_share.sublime-settings'
+
+def set_two_columns(view):
+    window = view.window()
+    if not window:
+        return
+
+    layout = window.get_layout()
+
+    if layout != TWO_COL_LAYOUT:
+        print("TwoColumns: layout is different")
+        window.set_layout(TWO_COL_LAYOUT)
+    else:
+        print("TwoColumns: layout is the same")
 
 
 class TwoColumns(sublime_plugin.EventListener):
-
     def on_new_async(self, view):
-        print("on_new_async called")
-        self._set_two_columns(view)
+        set_two_columns(view)
 
     def on_load_async(self, view):
-        print("on_load_async called")
-        self._set_two_columns(view)
+        set_two_columns(view)
 
     def __call__(self):
-        print("plugin_loaded called")
         view = sublime.active_window().active_view()
-        self._set_two_columns(view)
-
-    def _set_two_columns(self, view):
-        window = view.window()
-        if not window:
-            return
-
-        layout = window.get_layout()
-
-        if layout != TWO_COL_LAYOUT:
-
-            print("layout is different")
-            window.set_layout(TWO_COL_LAYOUT)
-        else:
-            print("layout is the same")
+        set_two_columns(view)
 
 
 # this is run at sublime text startup
@@ -48,24 +42,25 @@ def plugin_loaded():
 class CloneFileToPaneCommand(sublime_plugin.WindowCommand):
     def is_duplicated(self):
         active_buf = self.window.active_view().buffer_id()
-        dupe_bufs = [view
-                     for view in self.window.views()
-                     if view.buffer_id() == active_buf]
+        dupe_bufs = [
+            view for view in self.window.views()
+            if view.buffer_id() == active_buf
+        ]
         return True if len(dupe_bufs) > 1 else False
 
     def clone_file_to_pane(self):
         if not self.window.active_view():
             # If we're in an empty group, there's no active view
-            print("Empty group. Nothing to clone.")
+            print("TwoColumns: Empty group. Nothing to clone.")
             return
         if not self.is_duplicated():
-            print("No duplicates. Cloning.")
+            print("TwoColumns: No duplicates. Cloning.")
             original = self.window.active_view()
             self.window.run_command("clone_file")
             self.window.run_command("move_to_neighboring_group")
             self.window.focus_view(original)  # set focus back to original
         else:
-            print("Duplicates found.")
+            print("TwoColumns: Duplicates found. Not cloning")
 
     def run(self):
         self.clone_file_to_pane()
